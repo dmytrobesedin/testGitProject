fe59a2057450c89154a0442cbf79590767625d4e
dmytrobesedin <dbesedin7@gmail.com>
2022-05-12 12:12:20 +0300

    Initial Commit

---
+++ testGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGitProject
+//
+//  Created by Дмитрий Беседин on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
