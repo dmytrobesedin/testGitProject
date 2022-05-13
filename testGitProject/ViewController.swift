3cc11164483f825b4c4d1bb163387194db7d9c04
dmytrobesedin <dbesedin7@gmail.com>
2022-05-13 23:35:41 +0300

    Fix broken

--- testGitProject/ViewController.swift
+++ testGitProject/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     var uibutton = UIButton(frame: .infinite)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        uibutton.titleLabel?.text = "ok"
         view.addSubview(uibutton)
         // Do any additional setup after loading the view.
     }
