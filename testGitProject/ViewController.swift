83f7584856bb82c9f348b09f3fc4abf1a5bc6076
dmytrobesedin <dbesedin7@gmail.com>
2022-05-14 17:10:33 +0300

    mylabel

--- testGitProject/ViewController.swift
+++ testGitProject/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var uibutton = UIButton(frame: .infinite)
-    var label = UILabel(frame: .zero)
+    var myLabel = UILabel(frame: .infinite)
     override func viewDidLoad() {
         super.viewDidLoad()
         uibutton.titleLabel?.text = "ok"
