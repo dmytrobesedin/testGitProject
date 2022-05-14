5b451e48b3d2270c5e7118555e34c0d6a6d8a857
dmytrobesedin <dbesedin7@gmail.com>
2022-05-14 17:11:43 +0300

    add anotherLabel

--- testGitProject/ViewController.swift
+++ testGitProject/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var uibutton = UIButton(frame: .infinite)
-    var label = UILabel(frame: .null)
+    var anotherLabel = UILabel(frame: .infinite)
     override func viewDidLoad() {
         super.viewDidLoad()
         uibutton.titleLabel?.text = "ok"
